b3e585b88f414ce36114429b6a545322467b948c
Thorsten Kampe <thorsten@thorstenkampe.de>
2014-09-24 15:12:14 +0200

    Initiale Version

---
+++ python/script/_init.py
@@ -0,0 +1,119 @@
+##region## VARIABLES
+import sys, os
+
+script        = sys.argv[0]
+scriptpath    = os.path.dirname(script)
+scriptname    = os.path.basename(script)
+
+isPython2     = sys.version_info.major == 2
+isPyInstaller = getattr(sys, 'frozen', None)
+#endregion
+
+##region## EXTERNAL MODULE DEPENDENCIES
+if isPython2:
+    import imp
+else:
+    import importlib.util
+
+modules   = ['colorama', 'colorlog', 'crcmod', 'docopt']
+not_found = []
+modulemsg = (
+'Required external module(s) not found! You can install the package(s) with...',
+'',
+'pip install [--target "{scriptpath}"] {module}'
+)
+
+for module in modules:
+    if isPython2:
+        try:
+            imp.find_module(module)
+        except ImportError:
+            not_found.append(module)
+    else:
+        if importlib.util.find_spec(module) is None:
+            not_found.append(module)
+
+# PyInstaller includes all dependencies
+if not_found and not isPyInstaller:
+    sys.exit('\n'.join(modulemsg).format(scriptpath = scriptpath,
+                                         module     = " ".join(not_found)))
+#endregion
+
+##region## IMPORTS
+import logging, colorama, colorlog  ## LOGGING
+import traceback                    ## TRACEBACK
+import gettext                      ## INTERNATIONALIZATION
+import inspect                      ## DEBUGGING
+import binascii, time, crcmod       ## VERSION
+#endregion
+
+##region## LOGGING
+colorama.init()
+
+logger  = logging.getLogger()
+handler = logging.StreamHandler()
+if isPython2:
+    logmsg = '%(log_color)s%(levelname)s:%(reset)s %(message)s'
+else:
+    logmsg = '{log_color}{levelname}:{reset} {message}'
+
+handler.setFormatter(colorlog.ColoredFormatter(logmsg, style = '{'))
+logger.addHandler(handler)
+
+# set default log level to `info` so info messages can be seen
+logger.setLevel(logging.INFO)
+#endregion
+
+##region## TRACEBACK
+def _notraceback(type, value, trace_back):
+    logger.critical(''.join(traceback.format_exception_only(type, value)).rstrip())
+
+# no tracebacks, just the exception on error
+sys.excepthook = _notraceback
+
+if os.getenv('DEBUG') is not None:
+    # enable tracebacks for internal development
+    try:
+        import colored_traceback.auto
+    except ImportError:
+        logger.info('Install `colored_traceback` for colored tracebacks')
+#endregion
+
+##region## INTERNATIONALIZATION
+gettext.install(scriptname, localedir = os.path.join(scriptpath, '_translations'))
+#endregion
+
+##region## DEBUGGING
+def _traceit(frame, event, arg):
+    tracemsg = '+[{lineno}]: {code}'
+
+    if (frame.f_globals['__name__'] == '__main__' and
+        event in ['call', 'line']):
+
+        code_context = inspect.getframeinfo(frame).code_context[0].rstrip()
+
+        logger.debug(tracemsg.format(lineno = frame.f_lineno,
+                                     code   = code_context))
+
+    return _traceit
+
+def setupdebugging(debug):
+    if debug is True:
+        logger.setLevel(logging.DEBUG)
+        sys.settrace(_traceit)
+#endregion
+
+##region## VERSION
+# version is DATE.TIME.CHECKSUM (YYMMDD.HHMM_UTC.CRC-8_HEX)
+version_msg       = '{scriptname} {date}.{time}.{crc:02x}'
+modification_time = time.gmtime(os.path.getmtime(script))
+version_date      = time.strftime('%y%m%d', modification_time)
+version_time      = time.strftime('%H%M', modification_time)
+
+scriptfile        = open(script, 'rb').read()
+
+version_msg       = version_msg.format(scriptname = scriptname,
+                                       date       = version_date,
+                                       time       = version_time,
+                                       crc        = crcmod.predefined.mkCrcFun('crc-8')(scriptfile))
+#endregion
---
+++ python/script/script.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+"""
+{script} does something
+
+Usage:
+ {script} [-d]
+
+Options:
+ -d, --debug     show debug messages
+ -h, --help      show help
+ -v, --version   show version
+"""
+
+import _init, docopt
+
+arguments = docopt.docopt(_(__doc__.format(script = _init.scriptname)),
+                          version = _init.version_msg)
+
+_init.setupdebugging(arguments['--debug'])
+
+def main():
+    ##region## MAIN CODE STARTS HERE
+    pass
+    #endregion
+
+main()
