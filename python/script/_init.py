2a17aafc6b362f6c065f67092d331e20736f261e
Thorsten Kampe <thorsten@thorstenkampe.de>
2014-09-25 01:38:14 +0200

    Version um Betriebssystem-Version ergänzt

--- python/script/_init.py
+++ python/script/_init.py
@@ -9,11 +9,14 @@ isPython2     = sys.version_info.major == 2
 isPyInstaller = getattr(sys, 'frozen', None)
 #endregion
 
-##region## EXTERNAL MODULE DEPENDENCIES
-if isPython2:
-    import imp
-else:
+##region## IMPORTS
+
+# before importing we check for external module dependencies
+import imp
+try:
     import importlib.util
+except ImportError:
+    pass
 
 modules   = ['colorama', 'colorlog', 'crcmod', 'docopt']
 not_found = []
@@ -24,22 +27,21 @@ modulemsg = (
 )
 
 for module in modules:
-    if isPython2:
+
+    try:
+        if importlib.util.find_spec(module) is None:
+            not_found.append(module)
+    except (AttributeError, NameError):  # fallback for Python2 and Python3 < 3.4
         try:
             imp.find_module(module)
         except ImportError:
-            not_found.append(module)
-    else:
-        if importlib.util.find_spec(module) is None:
             not_found.append(module)
 
 # PyInstaller includes all dependencies
 if not_found and not isPyInstaller:
     sys.exit('\n'.join(modulemsg).format(scriptpath = scriptpath,
                                          module     = " ".join(not_found)))
-#endregion
 
-##region## IMPORTS
 import logging, colorama, colorlog       ## LOGGING
 import traceback                         ## TRACEBACK
 import gettext                           ## INTERNATIONALIZATION
@@ -105,16 +107,26 @@ def setupdebugging(debug):
 
 ##region## VERSION
 # script version is DATE.TIME.CHECKSUM (YYMMDD.HHMM_UTC.CRC-8_HEX)
-version_msg       = '{scriptname} {date}.{time}.{crc:02x} (Python {version})'
+version_msg       = '{scriptname} {date}.{time}.{crc:02x} (Python {version} on {platform})'
 modification_time = time.gmtime(os.path.getmtime(script))
 version_date      = time.strftime('%y%m%d', modification_time)
 version_time      = time.strftime('%H%M', modification_time)
 
 scriptfile        = open(script, 'rb').read()
 
-version_msg       = version_msg.format(scriptname = scriptname,
-                                       date       = version_date,
-                                       time       = version_time,
-                                       crc        = crcmod.predefined.mkCrcFun('crc-8')(scriptfile),
-                                       version    = platform.python_version())
+if sys.platform == 'win32':
+    os_platform = 'Windows {release}'.format(release = platform.release())
+
+elif sys.platform.startswith('linux'):
+    os_platform = '{distribution}'.format(distribution = ' '.join(platform.linux_distribution()[:2]))
+
+elif sys.platform == 'cygwin':
+    os_platform = 'Cygwin'
+
+version_msg = version_msg.format(scriptname = scriptname,
+                                 date       = version_date,
+                                 time       = version_time,
+                                 crc        = crcmod.predefined.mkCrcFun('crc-8')(scriptfile),
+                                 version    = platform.python_version(),
+                                 platform   = os_platform)
 #endregion
