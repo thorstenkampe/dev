f108456b6ac03edf6ebb13c6cb7c6476dd6d9fad
Thorsten Kampe <thorsten@thorstenkampe.de>
2014-10-03 15:11:32 +0200

    - `examples` ist jetzt Teil von `testing` - Importe mit `_` damit sie nicht in Tab-Vervollständigung auftauchen - `ishashable` und `isorderable` um `keyfunc` erweitert

--- python/modules/testing.py
+++ python/modules/testing.py
@@ -1,7 +1,10 @@
-from __future__ import division, print_function, unicode_literals
-import random, gc, time
+from __future__ import (division as _division,
+                        print_function as _print_function,
+                        unicode_literals as _unicode_literals)
+import random as _random, gc as _gc, time as _time,\
+       toolbox as _toolbox
 
-##region##
+##region TESTING FUNCTIONS##
 def dim(seq):
     dimension = []
     while isinstance(seq, (list, tuple)):
@@ -13,38 +16,38 @@ def dim(seq):
     return dimension
 
 def even(integer):
-    return not(integer % 2)
-
-def ishashable(seq):
-    try:
-        dict(zip(seq, range(len(seq))))
-    except TypeError:
-        return False
-    else:
-        return True
-
-def isorderable(seq):
-    try:
-        seq.sort()
-    except TypeError:
-        return False
-    else:
-        return True
+    return not(odd(integer))
 
 def odd(integer):
     return bool(integer % 2)
+
+def ishashable(seq, keyfunc = _toolbox.ident):
+    try:
+        dict(zip(map(keyfunc, seq), range(len(seq))))
+    except TypeError:
+        return False
+    else:
+        return True
+
+def isorderable(seq, keyfunc = _toolbox.ident):
+    try:
+        seq.sort(key = keyfunc)
+    except TypeError:
+        return False
+    else:
+        return True
 #endregion
 
-##region##
+##region UTILITIES##
 def randseq(start, end, count = None, repeat = False):
     if count is None:
         count = end - start + 1
 
     if repeat is False:
-        return random.sample(range(start, end + 1), count)
+        return _random.sample(range(start, end + 1), count)
 
     elif repeat is True:
-        return [random.randint(start, end) for counter in range(count)]
+        return [_random.randint(start, end) for counter in range(count)]
 
 def rows(table):
     try:
@@ -66,13 +69,43 @@ def timer(iteration, *func_and_args):
         iteration, function, args = 1, iteration, func_and_args
 
     iteration        = range(iteration)
-    gc.collect()  # force garbage collection
-    start_time_cpu   = time.clock()
-    start_time_total = time.time()
+    _gc.collect()  # force garbage collection
+    start_time_cpu   = _time.clock()
+    start_time_total = _time.time()
 
     for index in iteration:
         function(*args)
 
-    print('cpu: %.3f'   % (time.clock() - start_time_cpu),
-          'total: %.3f' % (time.time()  - start_time_total))
+    print('cpu: %.3f'   % (_time.clock() - start_time_cpu),
+          'total: %.3f' % (_time.time()  - start_time_total))
+#endregion
+
+##region TEST TYPES##
+smalldict    = {1: 11, '2': 22, 3: '33', '4': '44'}
+
+bigfile      = 'F:/Program Files/tools/cain/Wordlists/Wordlist.txt'
+
+smalllist    = ['a', 'b', 'c', 'd', 'e']
+biglist      = range(100)
+
+smallstring  = 'The quick brown fox jumps over the lazy dog'
+bigstring    = open(bigfile).read()
+
+smalltuple   = (11, 22, 33, 44)
+bigtuple     = tuple(range(100))
+
+table        = [['a1', 'b1', 'c1', 'd1', 'e1'],
+                ['a2', 'b2', 'c2', 'd2', 'e2'],
+                ['a3', 'b3', 'c3', 'd3', 'e3'],
+                ['a4', 'b4', 'c4', 'd4', 'e4']]
+#endregion
+
+##region HASH AND SORT##
+hashable     = [11, '22', 33]
+
+nonhashable  = [11, [22], 33]
+
+orderable    = [[11], [22], [33]]
+
+nonorderable = [11, ['22'], 33]
 #endregion
