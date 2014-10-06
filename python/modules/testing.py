85dbba633e9edab3593acf9a679c2fd7d230ad08
Thorsten Kampe <thorsten@thorstenkampe.de>
2014-10-07 01:04:25 +0200

    `timer` archiviert; `%timeit` stattdessen verwenden

--- python/modules/testing.py
+++ python/modules/testing.py
@@ -45,9 +45,7 @@ def isorderable(seq, keyfunc = _ident):
 #endregion
 
 ##region UTILITIES##
-import random as _random, \
-       gc     as _gc,     \
-       time   as _time
+import random as _random
 
 def randseq(start, end, count = None, repeat = False):
     if count is None:
@@ -65,29 +63,6 @@ def rows(table):
             print('%*s:' % (len(str(len(table))), lineno + 1), row)
     except TypeError:
         print(table)
-
-def timer(iteration, *func_and_args):
-    """
-    print the time elapsed (in seconds) evaluating function iteration
-    times (default is '1')
-    """
-    if isinstance(iteration, int):
-        function, args = func_and_args[0], func_and_args[1:]
-    else:
-        # if first argument is not a number, set function to iteration and
-        # iteration to '1'
-        iteration, function, args = 1, iteration, func_and_args
-
-    iteration        = range(iteration)
-    _gc.collect()  # force garbage collection
-    start_time_cpu   = _time.clock()
-    start_time_total = _time.time()
-
-    for index in iteration:
-        function(*args)
-
-    print('cpu: %.3f'   % (_time.clock() - start_time_cpu),
-          'total: %.3f' % (_time.time()  - start_time_total))
 #endregion
 
 ##region TEST TYPES##
