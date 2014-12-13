7990418f0d981088319123f78b9eaf950957e12a
Thorsten Kampe <thorsten@thorstenkampe.de>
2014-12-13 13:30:33 +0100

    Modul-Versionen wieder entfernt

--- python/script/_init.py
+++ python/script/_init.py
@@ -9,11 +9,11 @@ scriptname = os.path.basename(script)
 
 ##region IMPORTS ##
 try:
-    import logging, colorama, colorlog               ## LOGGING
-    import traceback, colored_traceback              ## TRACEBACK
-    import gettext                                   ## INTERNATIONALIZATION
-    import inspect, platform, pkg_resources, docopt  ## DEBUGGING
-    import time, crcmod                              ## VERSION
+    import logging, colorama, colorlog   ## LOGGING
+    import traceback, colored_traceback  ## TRACEBACK
+    import gettext                       ## INTERNATIONALIZATION
+    import inspect, platform             ## DEBUGGING
+    import time, crcmod                  ## VERSION
 except ImportError as exception:
     sys.exit('ERROR: {exception}'.format(exception = exception))
 #endregion
@@ -69,9 +69,6 @@ def setupdebugging(debug):
         arch     = platform.architecture()[0],
         platform = os_platform))
 
-    logger.debug('{module_versions}'.format(
-                     module_versions = ', '.join(modules)))
-
 # OS version
 if sys.platform == 'win32':
     os_platform = 'Windows {release}'.format(
@@ -88,20 +85,6 @@ elif sys.platform == 'cygwin':
 elif sys.platform == 'darwin':
     os_platform = 'OSX {release}'.format(
                       release = platform.mac_ver()[0])
-
-# module versions
-modules = ['colorama', 'colorlog', 'crcmod', 'docopt', 'colored_traceback']
-
-for index, module in enumerate(modules):
-    try:
-        modules[index] += ' {version}'.format(
-                              version = pkg_resources.get_distribution(module).version)
-    except pkg_resources.DistributionNotFound:
-        try:
-            modules[index] += ' {version}'.format(
-                                  version = sys.modules[module].__version__)
-        except AttributeError:
-            pass
 #endregion
 
 ##region VERSION ##
