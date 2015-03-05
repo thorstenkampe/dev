97781f7b30eb58c88c1b9d2228e1a41e39683686
Thorsten Kampe <thorsten@thorstenkampe.de>
2015-03-05 13:05:18 +0100

    - Fehler wenn Shell weder bash noch zsh - Farbe beim Debugging herausgenommen - `gettext` wird jetzt vorausgesetzt - Version ist Mercurial Revision Number

--- shell/script/_init.sh
+++ shell/script/_init.sh
@@ -1,13 +1,28 @@
-##
+## SHELL ##
 if [[ $OSTYPE = cygwin ]]      # `ps` is `procps` on Cygwin
 then
-    shell=$(procps --pid $$ --format comm=)
+    shell=$(procps --pid $$ \
+                   --format comm=)
 else
-    shell=$(ps --pid $$ --format comm=)
+    shell=$(ps --pid $$     \
+               --format comm=)
+fi
+
+if [[ $shell = bash ]]
+then
+    is_bash=true
+elif [[ $shell = zsh ]]
+then
+    is_bash=false
+else
+    { printf "ERROR: Shell \`$shell\` is not supported.\n"
+      printf 'Only `bash` and `zsh` are supported.\n'
+    } > /dev/stderr
+    exit 2                     # indicates "incorrect usage"
 fi
 
 ## OPTIONS ##
-if [[ $shell = bash ]]
+if $is_bash
 then
     shopt -os errexit nounset  # stop when an error occurs
 else
@@ -17,48 +32,41 @@ fi
 IFS=                           # disable word splitting (zsh: for command substitution)
 
 ## DEBUGGING ##
-if [[ $shell = bash ]]
-then
-    lcyan=$'\e[1;36m'
-    reset=$'\e[m'
-    PS4='\[$lcyan\]+\[$reset\]$(basename $BASH_SOURCE)${FUNCNAME+:$FUNCNAME}[$LINENO]\[$lcyan\]:\[$reset\] '
+debug() {
+    decimal_point=$(locale --category-name \
+                           --keyword-name  \
+                           decimal_point)
+    platform="$(uname --operating-system) $(uname --machine)"
 
-    function debug {
-    { printf "${lcyan}DEBUG:$reset %s\n" $(locale --category-name \
-                                                  --keyword-name  \
-                                                  decimal_point)
-      printf "${lcyan}DEBUG:$reset Trace\n";} >&2
-    shopt -os xtrace
-    }
-else
-    PS4='%B%F{cyan}+%b%f%1N[%I]%B%F{cyan}:%b%f '
+    if $is_bash
+    then
+        shell_version=$(printf %s.%s.%s ${BASH_VERSINFO[@]:0:3})
+        PS4='+$(basename $BASH_SOURCE)${FUNCNAME+:$FUNCNAME}[$LINENO]: '
+    else
+        shell_version=$ZSH_VERSION
+        PS4='+%1N[%I]: '
+    fi
 
-    function debug {
-    {print -P "%B%F{cyan}DEBUG:%b%f%s" $(locale --category-name \
-                                                --keyword-name  \
-                                                decimal_point)
-     print -P "%B%F{cyan}DEBUG:%b%f Trace"} >&2
-    trap "setopt xtrace" EXIT
-    }
-fi
+    { printf "DEBUG: $shell $shell_version on $platform\n"
+      printf "DEBUG: $decimal_point\n"
+      printf "DEBUG: Trace\n"
+    } > /dev/stderr            # `> /dev/stderr` is equivalent to `>&2`
+
+    if $is_bash
+    then
+        shopt -os xtrace
+    else
+        trap "setopt xtrace" EXIT
+    fi
+}
 
 ## INTERNATIONALIZATION ##
 # - http://www.gnu.org/software/gettext/manual/gettext.html#sh
 export TEXTDOMAINDIR=$(dirname $script)/_translations \
        TEXTDOMAIN=$scriptname
 
-if ! which gettext &> /dev/null
-then
-    function gettext {
-    printf $*
-    }
-fi
-
 ## VERSION ##
-# - version is MODIFICATION_DATE.TIME.FILE_CHECKSUM (YYMMDD.HHMM_UTC.CRC-16_HEX)
-# - bash needs `$((10#...)` because it treats numbers with leading zeroes as octal
-version="\
-$scriptname \
-$(date --reference $script --utc +%y%m%d.%H%M).\
-$(printf %04x $((10#$(sum $script | cut --fields 1 --delimiter " "))))\
-"
+# version is the Mercurial revision number
+version() {
+    printf "$scriptname %s\n" ${VERSION:11:3}
+}
