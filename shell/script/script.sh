00f97a9dd45f1a76ea258fa7d17ca474d79ef91a
Thorsten Kampe <thorsten@thorstenkampe.de>
2015-04-07 08:47:10 +0200

    Beispiel fuer Option hinzugefuegt

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -11,19 +11,23 @@ Usage:
  $scriptname
 
 Options:
-"
-# options `-d`, `-v`, and `-h` are always available
+ -o   do optional stuff
+"                            # `-d`, `-v`, and `-h` are always available
 
 script=$0
 source "$(dirname "$0")"/_init.sh
 
-while getopts dhv option  # option string needs standard options `dhv`
+declare -A opts
+while getopts o:dhv option   # option string needs standard options `dhv`
 do
     case $option in
-        \?)               # literal `?` indicates unknown option
+        o)                   # test if option is set with
+            opts[o]=$OPTARG  # `if [[ ${opts[o]+set} ]]`
+            ;;
+        \?)                  # literal `?` indicates unknown option
             exit 1
     esac
 done
-shift $((OPTIND - 1))     # remove options from command line
+shift $((OPTIND - 1))        # remove options from command line
 
 ## MAIN CODE STARTS HERE ##
