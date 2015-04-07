dd803272d0e8d1c4532c1ae5945d304345c081ff
Thorsten Kampe <thorsten@thorstenkampe.de>
2015-04-07 08:49:08 +0200

    Reformatierung

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -12,22 +12,22 @@ Usage:
 
 Options:
  -o   do optional stuff
-"                            # `-d`, `-v`, and `-h` are always available
+"                           # `-d`, `-v`, and `-h` are always available
 
 script=$0
 source "$(dirname "$0")"/_init.sh
 
 declare -A opts
-while getopts o:dhv option   # option string needs standard options `dhv`
+while getopts o:dhv option  # option string needs standard options `dhv`
 do
     case $option in
-        o)                   # test if option is set with
-            opts[o]=$OPTARG  # `if [[ ${opts[o]+set} ]]`
+        o)                  # test if option is set with `if [[ ${opts[o]+set} ]]`
+            opts[o]=$OPTARG
             ;;
-        \?)                  # literal `?` indicates unknown option
+        \?)                 # literal `?` indicates unknown option
             exit 1
     esac
 done
-shift $((OPTIND - 1))        # remove options from command line
+shift $((OPTIND - 1))       # remove options from command line
 
 ## MAIN CODE STARTS HERE ##
