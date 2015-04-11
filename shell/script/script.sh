d61d94d25e315a7b76fa66bf12dd1d738b0cc0d5
Thorsten Kampe <thorsten@thorstenkampe.de>
2015-04-11 15:47:05 +0200

    `-O` Beispiel-Option entfernt

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -3,25 +3,20 @@
 VERSION='$Revision$'
 DATE='$Date$'
 
-description="does something"  # prints "`SCRIPT` DESCRIPTION"
-usage="[-O]"                  # prints "Usage: SCRIPT USAGE"
-options_help="\
- -O   do optional stuff"      # prints "Options: OPTIONS_HELP"
+description=              # prints "`SCRIPT` DESCRIPTION"
+usage=                    # prints "Usage: SCRIPT USAGE"
+options_help=             # prints "Options:\nOPTIONS_HELP"
 
 script=$0
 source "$(dirname "$0")"/_init.sh
 
-while getopts O:dhv option    # option string needs standard options `dhv`
+while getopts dhv option  # option string needs standard options `dhv`
 do
     case $option in
-        O)                    # test if option is set with `if [[ ${opts[O]+set} ]]`
-            opts[O]=$OPTARG
-            ;;
-
         \?)
             exit 1
     esac
 done
-shift $((OPTIND - 1))         # remove options from command line
+shift $((OPTIND - 1))     # remove options from command line
 
 ## MAIN CODE STARTS HERE ##
