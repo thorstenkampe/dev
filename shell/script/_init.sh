1233aa6e50cfc59777fffde69bce292e875e4f71
Thorsten Kampe <thorsten@thorstenkampe.de>
2016-12-28 19:18:47 +0100

    - Unterstützung für Bash3 wieder hinzugefügt (wegen macOS)

--- shell/script/_init.sh
+++ shell/script/_init.sh
@@ -1,4 +1,4 @@
-## - short instead of long options are used for OS X compatibility
+## - short instead of long options are used for macOS compatibility
 ## - we don't use `> /dev/stderr` instead of `>&2` because of problems
 ##   with the implementation on Cygwin
 
@@ -7,21 +7,17 @@ IFS=  # disable word splitting
 
 # functions mostly used for debugging shouldn't run on normal execution
 isCygwin() { [[ $OSTYPE == cygwin ]]; }
-isLinux()  { [[ $OSTYPE == linux-gnu ]]; }
-isOSX()    { [[ $OSTYPE =~ ^darwin ]]; }
-isUbuntu() { [[ -e /etc/lsb-release ]]; }
-isRedHat() { [[ -e /etc/redhat-release ]]; }
-isSUSE()   { [[ -e /etc/SuSE-release ]]; }
 
 if isCygwin  # `ps` is `procps` on Cygwin
 then
     shell=$(procps --pid $$ --format comm=)
 else
-    # `ps` shows full path on OS X
+    # `ps` shows full path on macOS
     shell=$(basename $(ps -p $$ -o comm=))
 fi
 
-isBash()  { [[ $shell == bash ]]; }
+isBash() { [[ $shell == bash ]]; }
+isBash3() { isBash && ((BASH_VERSINFO <= 3)); }
 
 if isBash
 then
@@ -50,32 +46,40 @@ then
 fi
 
 ## LOGGING ##
-declare -A loglevel color
+if isBash3
+then
+    # No associative arrays in Bash 3, so only rudimentary logging
+    log() {
+        printf "%s: %s\n" $1 $2 >&2
+    }
+else
+    declare -A loglevel color
 
-# Modeled after Python modules `logging` and `colorlog`
-verbosity=WARNING  # default level
+    # Modeled after Python modules `logging` and `colorlog`
+    verbosity=WARNING  # default level
 
-# For color codes see http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-loglevel[CRITICAL]=10 ; color[CRITICAL]=$'\e[1;31m'  # brightred
-loglevel[ERROR]=20    ; color[ERROR]=$'\e[0;31m'     # red
-loglevel[WARNING]=30  ; color[WARNING]=$'\e[0;33m'   # yellow
-loglevel[INFO]=40     ; color[INFO]=$'\e[0;32m'      # green
-loglevel[DEBUG]=50    ; color[DEBUG]=$'\e[0;37m'     # white
+    # For color codes see http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    loglevel[CRITICAL]=10  color[CRITICAL]=$'\e[1;31m'  # brightred
+    loglevel[ERROR]=20     color[ERROR]=$'\e[0;31m'     # red
+    loglevel[WARNING]=30   color[WARNING]=$'\e[0;33m'   # yellow
+    loglevel[INFO]=40      color[INFO]=$'\e[0;32m'      # green
+    loglevel[DEBUG]=50     color[DEBUG]=$'\e[0;37m'     # white
 
-reset=$'\e[m'
+    reset=$'\e[m'
 
-log() {
-    if ((${loglevel[$1]} <= ${loglevel[$verbosity]}))
-    then
-        # only output color if stderr is attached to tty
-        if [[ -t 2 ]]
+    log() {
+        if ((${loglevel[$1]} <= ${loglevel[$verbosity]}))
         then
-            printf "%s%s:%s %s\n" ${color[$1]} $1 $reset $2 >&2
-        else
-            printf "%s: %s\n" $1 $2 >&2
+            # only output color if stderr is attached to tty
+            if [[ -t 2 ]]
+            then
+                printf "%s%s:%s %s\n" ${color[$1]} $1 $reset $2 >&2
+            else
+                printf "%s: %s\n" $1 $2 >&2
+            fi
         fi
-    fi
-}
+    }
+fi
 
 ## VERSION ##
 shell_version() {
@@ -84,46 +88,6 @@ shell_version() {
         printf "%s.%s.%s" ${BASH_VERSINFO[@]:0:3}
     else
         printf "%s" $ZSH_VERSION
-    fi
-}
-
-os_version() {
-    if   isCygwin
-    then
-        osver=$(uname --kernel-release)
-        printf "Cygwin %s" ${osver%\(*}
-
-    elif isOSX
-    then
-        printf "OS X %s" \
-        $(python -c 'import platform; print(platform.mac_ver()[0])')
-
-    elif isUbuntu
-    then
-        source /etc/lsb-release
-        printf "%s" $DISTRIB_DESCRIPTION
-
-    # RHEL, XENSERVER
-    elif isRedHat
-    then
-        awk '
-{NF--  # print file except last field
- print}' \
-            /etc/redhat-release
-
-    # SLES
-    elif isSUSE
-    then
-        awk '
-NR == 1 {NF--       # print first line except last field
-         printf "%s SP ", $0}
-NR == 3 {print $3}  # print third field from third line' \
-            /etc/SuSE-release
-
-    # OTHER LINUX DISTRIBUTION
-    elif isLinux
-    then
-        printf "Linux"
     fi
 }
 
@@ -179,8 +143,7 @@ trap error_handler ERR INT HUP QUIT TERM
 if [[ -n ${DEBUG-} ]]
 then
     verbosity=DEBUG
-    log DEBUG $(printf "%s %s on %s %s" \
-                $shell $(shell_version) $(os_version) $(uname -m))
+    log DEBUG $(printf "%s %s" $shell $(shell_version))
 
     # https://www.gnu.org/software/gettext/manual/html_node/Locale-Environment-Variables.html
     # http://pubs.opengroup.org/onlinepubs/7908799/xbd/locale.html
