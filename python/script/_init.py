454950632803f4a9a064c8436bedcb195c5be7f5
Thorsten Kampe <thorsten@thorstenkampe.de>
2016-12-31 11:39:16 +0100

    `spinner()`, OS-Versionen und `colored_traceback` entfernt

--- python/script/_init.py
+++ python/script/_init.py
@@ -1,19 +1,17 @@
 ##region IMPORTS ##
 from __future__ import division, print_function, unicode_literals
-import sys                                                  # VARIABLES
+import sys                                          # VARIABLES
 try: import pathlib
 except ImportError: import pathlib2 as pathlib
-import signal # + sys                                       # CONSOLE
-from pycompat import python, system
-import colorama, colorlog, logging                          # LOGGING
-import gettext, locale # + pathlib                          # INTERNATIONALIZATION
-import colored_traceback, inspect, os, platform, traceback  # DEBUGGING
+import signal; from pycompat import system # + sys  # CONSOLE
+import colorama, colorlog, logging                  # LOGGING
+import gettext, locale # + pathlib                  # INTERNATIONALIZATION
+import inspect, os, platform, traceback             # DEBUGGING
        # + locale, logging, pycompat, sys
-import concurrent.futures, progress.spinner, time           # SPINNER
 #endregion
 
 ##region VARIABLES ##
-scriptpath    = pathlib.Path(sys.argv[0]).parent
+scriptparent  = pathlib.Path(sys.argv[0]).parent
 scriptname    = pathlib.Path(sys.argv[0]).name
 isPyinstaller = getattr(sys, 'frozen', None) == True
 #endregion
@@ -21,10 +19,11 @@ isPyinstaller = getattr(sys, 'frozen', None) == True
 ##region CONSOLE ##
 # no traceback on Ctrl-C;
 # cleaning up on termination can be done with `atexit.register()`
-if system.is_windows:  # Windows has no `SIGHUP`
+if system.is_windows:  # Windows has no `SIGHUP` and `SIGQUIT`
     termsignals = (signal.SIGINT, signal.SIGTERM)
 else:
-    termsignals = (signal.SIGINT, signal.SIGTERM, signal.SIGHUP)
+    termsignals = (signal.SIGINT, signal.SIGTERM, signal.SIGHUP,
+                   signal.SIGQUIT)
 
 for termsignal in termsignals:
     signal.signal(termsignal, lambda *args: sys.exit())
@@ -45,28 +44,13 @@ logging.getLogger().addHandler(handler)
 ##region INTERNATIONALIZATION ##
 # `str()` superfluous in Python3
 gettext.install(
-    scriptname, localedir = str(pathlib.Path(scriptpath, '_translations')))
+    scriptname, localedir = str(pathlib.Path(scriptparent, '_translations')))
 
 # make Python locale aware
 locale.setlocale(locale.LC_ALL, '')
 #endregion
 
 ##region DEBUGGING ##
-# OS version
-if system.is_windows:
-    os_platform = ['Windows', platform.release()]
-
-elif system.is_linux:
-    os_platform = platform.linux_distribution()[:2]
-
-elif system.is_cygwin:
-    os_platform = ['Cygwin', platform.release()[:5]]
-
-elif system.is_mac_os:
-    os_platform = ['OSX', platform.mac_ver()[0]]
-
-os_platform = ' '.join(os_platform)
-
 def _notraceback(type, value, trace_back):
     logger.critical(
         ''.join(traceback.format_exception_only(type, value)).rstrip())
@@ -75,9 +59,8 @@ def _traceit(frame, event, arg):
     if (frame.f_globals['__name__'] == '__main__' and
         event in ['call', 'line']):
 
-        logger.debug('+[{lineno}]: {code}'.format(
-            lineno = frame.f_lineno,
-            code   = inspect.getframeinfo(frame).code_context[0].rstrip()))
+        logger.debug('+[%s]: %s', frame.f_lineno,
+            inspect.getframeinfo(frame).code_context[0].rstrip())
     return _traceit
 
 # During standard execution, we want no traceback, just the exception
@@ -87,35 +70,12 @@ sys.excepthook = _notraceback
 if os.getenv('PYTHONDEBUG') is not None:
     logger.setLevel(logging.DEBUG)
 
-    # enable (colored) tracebacks
-    colored_traceback.add_hook()
-
     if not (isPyinstaller and system.is_linux):
         sys.settrace(_traceit)
 
-logger.debug('Python %s %s on %s', platform.python_version(),
-                platform.architecture()[0], os_platform)
+logger.debug('Python %s %s', platform.python_version(), platform.architecture()[0])
 locale_vars = {key: os.environ.get(key, '') for key in
                    ('LANGUAGE', 'LC_ALL', 'LANG')}
 locale_vars['decimal_point'] = locale.localeconv()['decimal_point']
 logger.debug(locale_vars)
-#endregion
-
-##region SPINNER ##
-def show_spinner(*args):
-
-    spinner  = progress.spinner.Spinner()
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers = 1)
-    future   = executor.submit(*args)
-    # Python2 backport `pythonfutures` needs a delay after
-    # `executor.submit()` for `future.running()`
-    time.sleep(0.001)
-
-    while future.running():
-        spinner.next()
-        time.sleep(0.1)
-
-    executor.shutdown()
-    spinner.finish()
-    print()
 #endregion
