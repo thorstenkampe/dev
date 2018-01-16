1bb9d6d11adaab3e3f15fa43209e78871e3921ec
Thorsten Kampe <thorsten@thorstenkampe.de>
2018-01-16 08:06:15 +0100

    Initiale Version

---
+++ powershell/script.ps1
@@ -0,0 +1,30 @@
+#! /usr/bin/env pwsh
+
+<#
+.SYNOPSIS
+DESCRIPTION
+
+.DESCRIPTION
+`SCRIPT` DESCRIPTION
+#>
+
+[CmdletBinding(PositionalBinding = $false)]
+param(
+)
+
+## INITIALIZATION ##
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version latest
+
+## DEBUGGING ##
+# script is run with `-debug`
+if ($DebugPreference -eq "Inquire") {
+    $DebugPreference = "Continue"
+}
+
+Write-Debug -Message "PowerShell $($PSVersionTable.PSVersion.ToString())"
+if ($DebugPreference -eq "Continue") {
+    Set-PSDebug -Trace 1
+}
+
+## MAIN CODE STARTS HERE ##
