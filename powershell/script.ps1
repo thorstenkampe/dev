a4d6e66c853bf64f08c27376a3f2ef6df4bc3f4f
Thorsten Kampe <thorsten@thorstenkampe.de>
2018-01-19 22:09:39 +0100

    String mit Format-Operator

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -17,12 +17,12 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version latest
 
 ## DEBUGGING ##
-# script is run with `-debug`
+# script is run with `-Debug`
 if ($DebugPreference -eq 'Inquire') {
     $DebugPreference = 'Continue'
 }
 
-Write-Debug -Message "PowerShell $($PSVersionTable.PSVersion.ToString())"
+Write-Debug -Message ('PowerShell {0}' -f $PSVersionTable.PSVersion.ToString())
 if ($DebugPreference -eq 'Continue') {
     Set-PSDebug -Trace 1
 }
