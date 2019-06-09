3962bd37acf82c337a99f6eeacc908ddff2b334a
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-06-09 10:42:40 +0200

    reformatting

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -8,22 +8,20 @@ DESCRIPTION
 `SCRIPT` DESCRIPTION
 #>
 
-# INITIALIZATION #
 [CmdletBinding(SupportsShouldProcess)]
 Param([Switch] $Help)  # make help available the standard way (without `Get-Help`)
 
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version latest
 
-# HELP #
+# `-Help`
 if ($Help) {
     Get-Help -Name $MyInvocation.InvocationName -Full
     exit 1
 }
 
-# DEBUGGING #
+# `-Debug`
 Write-Debug -Message ('PowerShell {0} {1}' -f $PSEdition, $PSVersionTable.PSVersion)
-# script is run with `-Debug`
 if ($DebugPreference -eq 'Continue') {
     Set-PSDebug -Trace 1
 }
