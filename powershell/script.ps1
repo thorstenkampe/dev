775482e91b18b437b1e412fa1a0cf3f431b5d817
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-07-03 23:01:36 +0200

    disable `SupportsShouldProcess` unless it's used

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -8,8 +8,8 @@ DESCRIPTION
 SCRIPT DESCRIPTION
 #>
 
-[CmdletBinding(SupportsShouldProcess)]
-Param([Switch] $Help)  # make help available without `Get-Help`
+#[CmdletBinding(SupportsShouldProcess)]  # support for `ShouldProcess` and `ShouldContinue`
+Param([Switch] $Help)                    # make help available without `Get-Help`
 
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version latest
