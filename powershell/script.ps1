fa8e1f568b4ecb14c3cc71ef9e4ee7ad1b7a7272
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-07-03 18:53:18 +0200

    logging implemented

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -14,15 +14,30 @@ Param([Switch] $Help)  # make help available without `Get-Help`
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version latest
 
-# `-Help`
-if ($Help) {
+# LOGGING #
+# modeled after Python modules `logging` and `colorlog`
+$verbosity = 'WARNING'  # default level
+$loglevel  = @{CRITICAL = 10; ERROR = 20; WARNING = 30; INFO = 40; DEBUG = 50}
+$color     = @{CRITICAL = 'Red'; ERROR = 'DarkRed'; WARNING = 'DarkYellow'; INFO = 'DarkGreen'; DEBUG = 'Gray'}
+
+function log {
+    if ($loglevel[$args[0]] -le $loglevel[$verbosity]) {
+        Write-Host -Object $args[0] -ForegroundColor $color[$args[0]] -NoNewline
+        Write-Host -Object (": {0}" -f $args[1])
+    }
+}
+
+# OPTIONS #
+if     ($Help) {                              # `-Help`
     Get-Help -Name $MyInvocation.InvocationName -Full
     exit 1
 }
-
-# `-Debug`
-Write-Debug -Message ("PowerShell $PSEdition {0}" -f $PSVersionTable.PSVersion)
-if ($DebugPreference -eq 'Continue') {
+elseif ($VerbosePreference -eq 'Continue') {  # `-Verbose`
+    $verbosity = 'INFO'
+}
+elseif ($DebugPreference -eq 'Continue') {    # `-Debug`
+    $verbosity = 'DEBUG'
+    log DEBUG ("PowerShell $PSEdition {0}" -f $PSVersionTable.PSVersion)
     Set-PSDebug -Trace 1
 }
 
