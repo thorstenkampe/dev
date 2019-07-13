e3810f8e5a51a45a711e3a2801fb76aa301058a4
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-07-13 19:04:35 +0200

    Major rewrite: debugging via `bash -x`; help removed

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -1,25 +1,13 @@
-help='SCRIPT DESCRIPTION
-
-Usage:
- SCRIPT [options]
-
-Options:
- -h   show help
- -d   show debug messages'
-
 # INITIALIZATION #
+PS4='+$(basename $BASH_SOURCE)${FUNCNAME:+:$FUNCNAME}[$LINENO]: '
 shopt -os nounset pipefail errexit
-IFS=  # disable word splitting
-# https://www.gnu.org/software/gettext/manual/html_node/Locale-Environment-Variables.html
-export LANG=en_US.UTF-8  # "neutral" environment
+IFS=                     # disable word splitting
+export LANG=en_US.UTF-8  # neutral environment
 
 # LOGGING #
-# modeled after Python modules `logging` and `colorlog`
-verbosity=WARNING  # default level
+[[ -o xtrace ]] && verbosity=DEBUG || verbosity=WARNING
 declare -A loglevel colorcodes
 loglevel=([CRITICAL]=50 [ERROR]=40 [WARNING]=30 [INFO]=20 [DEBUG]=10)
-# for color codes see http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-# CRITICAL: brightred, ERROR: red, WARNING: yellow, INFO: green, DEBUG: white)
 colorcodes=([CRITICAL]='1;31' [ERROR]='0;31' [WARNING]='0;33' [INFO]='0;32' [DEBUG]='0;37')
 
 function log {
@@ -34,28 +22,4 @@ function log {
     fi
 }
 
-# DEFAULT OPTIONS #
-function default_options {
-    case $option in
-        (h)
-            echo "$help"
-            exit;;
-
-        (d)
-            PS4='+$(basename $BASH_SOURCE)${FUNCNAME:+:$FUNCNAME}[$LINENO]: '
-            verbosity=DEBUG
-
-            log DEBUG "bash $BASH_VERSION"
-            shopt -os xtrace;;
-
-        ('?')
-            exit 1
-    esac
-}
-
 # MAIN CODE STARTS HERE #
-while getopts hd option
-do
-    default_options
-done
-shift $((OPTIND - 1))  # remove options from command line
