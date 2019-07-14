ae1d684d4d891c4ec8a8c65d0f758a35cc559046
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-07-14 18:07:13 +0200

    test if color output outside of log function

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -5,20 +5,26 @@ IFS=                     # disable word splitting
 export LANG=en_US.UTF-8  # neutral environment
 
 # LOGGING #
-[[ -o xtrace ]] && verbosity=DEBUG || verbosity=WARNING
+if [[ -o xtrace ]]
+then
+    verbosity=DEBUG
+else
+    verbosity=WARNING
+fi
 declare -A loglevel colorcodes
 loglevel=([CRITICAL]=50 [ERROR]=40 [WARNING]=30 [INFO]=20 [DEBUG]=10)
-colorcodes=([CRITICAL]='1;31' [ERROR]='0;31' [WARNING]='0;33' [INFO]='0;32' [DEBUG]='0;37')
+if [[ -t 2 ]]  # only output color if stderr is attached to tty
+then
+    colorcodes=([CRITICAL]='\e[1;31m' [ERROR]='\e[0;31m' [WARNING]='\e[0;33m' [INFO]='\e[0;32m'
+                [DEBUG]='\e[0;37m' [RESET]='\e[m')
+else
+    colorcodes=([CRITICAL]= [ERROR]= [WARNING]= [INFO]= [DEBUG]= [RESET]=)
+fi
 
 function log {
     if ((loglevel[$1] >= loglevel[$verbosity]))
     then
-        if [[ -t 2 ]]  # only output color if stderr is attached to tty
-        then
-            echo -e "\e[${colorcodes[$1]}m$1\e[m: $2"
-        else
-            echo "$1: $2"
-        fi > /dev/stderr
+        echo -e "${colorcodes[$1]}$1${colorcodes[RESET]}: $2" > /dev/stderr
     fi
 }
 
