01efd0ffee2f1081976895ba5cf62a63a9a1fdcc
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-07-19 08:51:28 +0200

    logging more "streamlined" (shorter, more "expressive")

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -5,26 +5,16 @@ IFS=''                   # disable word splitting
 export LANG=en_US.UTF-8  # neutral environment
 
 # LOGGING #
-if [[ -o xtrace ]]
-then
-    verbosity=DEBUG
-else
-    verbosity=WARNING
-fi
-declare -A loglevel colorcodes
-loglevel=([CRITICAL]=50 [ERROR]=40 [WARNING]=30 [INFO]=20 [DEBUG]=10)
-if [[ -t 2 ]]  # only output color if stderr is attached to tty
-then
-    colorcodes=([CRITICAL]='\e[1;31m' [ERROR]='\e[0;31m' [WARNING]='\e[0;33m' [INFO]='\e[0;32m'
-                [DEBUG]='\e[0;37m' [RESET]='\e[m')
-else
-    colorcodes=([CRITICAL]='' [ERROR]='' [WARNING]='' [INFO]='' [DEBUG]='' [RESET]='')
-fi
+[[ -o xtrace ]] && verbosity=DEBUG || verbosity=WARNING
+declare -A loglevel=([CRITICAL]=50 [ERROR]=40 [WARNING]=30 [INFO]=20 [DEBUG]=10) \
+           color=([CRITICAL]='\e[1;31m' [ERROR]='\e[0;31m' [WARNING]='\e[0;33m' [INFO]='\e[0;32m' [DEBUG]='\e[0;37m' [RESET]='\e[m')
+# no color if stderr is not attached to tty
+[[ ! -t 2 ]] && color=([CRITICAL]='' [ERROR]='' [WARNING]='' [INFO]='' [DEBUG]='' [RESET]='')
 
 function log {
     if ((loglevel[$1] >= loglevel[$verbosity]))
     then
-        echo -e "${colorcodes[$1]}$1${colorcodes[RESET]}: $2" > /dev/stderr
+        echo -e "${color[$1]}$1${color[RESET]}: $2" > /dev/stderr
     fi
 }
 
