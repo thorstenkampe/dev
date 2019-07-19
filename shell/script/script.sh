6b71bb4d6243bb9c0cf48df3615ce01292f1a4aa
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-07-19 21:33:29 +0200

    empty line removed

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -3,7 +3,6 @@ PS4='+$(basename $BASH_SOURCE)${FUNCNAME:+:$FUNCNAME}[$LINENO]: '
 shopt -os nounset pipefail errexit
 IFS=''                   # disable word splitting
 export LANG=en_US.UTF-8  # neutral environment
-
 # * color codes: http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
 # * use `ansifilter` to discard color codes when redirecting to file
 declare -A colorcode=([red]='\e[31m' [green]='\e[32m' [yellow]='\e[33m' [white]='\e[37m'
