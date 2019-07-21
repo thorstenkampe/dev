c279b38da191cd9dfafb15e1074b6d8a3f7c097b
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-07-21 20:11:54 +0200

    help simplified, debugging/tracing removed

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -1,27 +1,8 @@
-﻿<#
-.SYNOPSIS
-DESCRIPTION
-
-.DESCRIPTION
-SCRIPT DESCRIPTION
-
-.PARAMETER Help
-show detailed help
-
-.PARAMETER WhatIf
-show what the script would do (dry run)
-
-.PARAMETER Confirm
-confirm the script's actions
-#>
-
-# INITIALIZATION #
-# support `-Verbose`, `-Debug`, `-WhatIf`, `-Confirm`, `ShouldProcess()`, and `ShouldContinue()`
-[CmdletBinding(SupportsShouldProcess)]
+﻿# INITIALIZATION #
+[CmdletBinding()]
 Param([Switch] $Help)  # make help available without `Get-Help`
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version latest
-# https://stackoverflow.com/questions/2379514/powershell-formatting-values-in-another-culture/
 [cultureinfo]::currentculture = [cultureinfo]::InvariantCulture  # "neutral" environment
 
 Import-Module -Name PSWriteColor
@@ -33,26 +14,14 @@ $color     = @{CRITICAL = 'Red'; ERROR = 'DarkRed'; WARNING = 'DarkYellow'; INFO
 
 function log($Level, $Message) {
     if ($loglevel[$Level] -ge $loglevel[$verbosity]) {
-        if ($verbosity -eq 'DEBUG') {
-            Write-Host -Object "${Level}: $Message"
-        }
-        else {
-            Write-Color -Text $Level, ": $Message" -Color $color[$Level], Gray
-        }
+        Write-Color -Text $Level, ": $Message" -Color $color[$Level], Gray
     }
 }
 
-# DEFAULT OPTIONS #
-if     ($Help) {                              # `-Help`
-    Get-Help -Name $MyInvocation.InvocationName -Detailed
+# OPTIONS #
+if ($Help) {  # `-Help`
+    Get-Help -Name $MyInvocation.InvocationName
     exit
-}
-elseif ($VerbosePreference -eq 'Continue') {  # `-Verbose`
-    $verbosity = 'INFO'
-}
-elseif ($DebugPreference -eq 'Continue') {    # `-Debug`
-    $verbosity = 'DEBUG'
-    Set-PSDebug -Trace 1
 }
 
 # MAIN CODE STARTS HERE #
