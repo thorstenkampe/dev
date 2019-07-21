424f2d8697701023fbdf1be70711eb5bd589a855
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-07-21 19:00:44 +0200

    tracing and debuggin removed; simplified

--- python/script/script.py
+++ python/script/script.py
@@ -1,15 +1,11 @@
 """
-SCRIPT DESCRIPTION
+script.py DESCRIPTION
 
 Usage:
- SCRIPT [-h|-d]
-
-Options:
- -h, --help    show help
- -d, --debug   show debug messages
+ script.py
 """
 
-import inspect, sys, colorlog, docpie
+import colorlog, docpie
 
 # LOGGING #
 logger  = colorlog.getLogger(name = '__main__')
@@ -18,24 +14,7 @@ handler = colorlog.StreamHandler()
 handler.setFormatter(colorlog.ColoredFormatter('%(log_color)s%(levelname)s%(reset)s: %(message)s'))
 logger.addHandler(handler)
 
-# DEBUGGING #
-def _traceit(frame, event, arg):
-    if frame.f_globals['__name__'] == '__main__' and event in ['call', 'line']:
-        logger.debug('+[%s]: %s', frame.f_lineno, inspect.getframeinfo(frame).code_context[0].rstrip())
-    return _traceit
-
-def _debug():
-    logger.setLevel('DEBUG')
-    sys.settrace(_traceit)
-
-# DEFAULT OPTIONS #
+# OPTIONS #
 arguments = docpie.docpie(__doc__)
 
-if arguments['--debug']:
-    _debug()
-
 # MAIN CODE STARTS HERE #
-def main():
-    pass
-
-main()
