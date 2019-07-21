fa81d6c570c70f729dc03aa687a8d7216e15fe74
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-07-22 00:09:39 +0200

    ...

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -7,7 +7,7 @@ shopt -os nounset pipefail errexit
 IFS=''                   # disable word splitting
 export LANG=en_US.UTF-8  # neutral environment
 
-# DEFAULT OPTIONS #
+# OPTIONS #
 while getopts h option
 do
     case $option in
