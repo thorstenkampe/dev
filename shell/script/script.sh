5ef2b62b0638674c975429e8eec8ea75060a60bc
Thorsten Kampe <thorsten@thorstenkampe.de>
2019-11-09 23:06:19 +0100

    BATS support

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -1,22 +1,28 @@
 help='Usage: script.sh'
 
 # INITIALIZATION #
-PS4='+$(basename $BASH_SOURCE)${FUNCNAME:+:$FUNCNAME}[$LINENO]: '
+PS4='+$(basename "${BASH_SOURCE[0]}")${FUNCNAME:+:$FUNCNAME}[$LINENO]: '
 shopt -os nounset pipefail errexit
 export LANG=en_US.UTF-8  # neutral environment
 
 # OPTIONS #
-while getopts h option
-do
-    case $option in
-        (h)
-            echo "$help"
-            exit;;
+function do_options {
+    while getopts h option
+    do
+        case $option in
+            (h)
+                echo "$help"
+                exit;;
 
-        ('?')
-            exit 1
-    esac
-done
-shift $((OPTIND - 1))  # remove options from command line
+            ('?')
+                exit 1
+        esac
+    done
+    shift $((OPTIND - 1))  # remove options from command line
+}
 
 # MAIN CODE STARTS HERE #
+if [[ ${BASH_SOURCE[0]} == "$0" ]]
+then
+    do_options "$@"
+fi
