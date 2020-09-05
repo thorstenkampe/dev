ee85af841f85c4857cc97789f09e635fbb1a25fc
Thorsten Kampe <thorsten@thorstenkampe.de>
2020-09-05 13:34:33 +0200

    switched from `docopt` to `click`

--- python/script/script.py
+++ python/script/script.py
@@ -1,10 +1,16 @@
-'''
-Usage: script.py
-'''
+import click              # https://click.palletsprojects.com/en/7.x/#documentation
+import click_help_colors  # https://github.com/click-contrib/click-help-colors
 
-import docopt
-
-# OPTIONS #
-arguments = docopt.docopt()
+@click.command(
+    context_settings   = {'help_option_names': ['-h', '--help']},
+    cls                = click_help_colors.HelpColorsCommand,
+    help_headers_color = 'cyan',
+    help_options_color = 'bright_white'
+)
 
 # MAIN CODE STARTS HERE #
+
+def main():
+    '''purpose of script'''
+
+main()
