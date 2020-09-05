4d319156591bc6f164ea1d623c17b9f3bf03ea29
Thorsten Kampe <thorsten@thorstenkampe.de>
2020-09-05 13:44:06 +0200

    new from-import style

--- python/script/script.py
+++ python/script/script.py
@@ -1,9 +1,9 @@
-import click              # https://click.palletsprojects.com/en/7.x/#documentation
-import click_help_colors  # https://github.com/click-contrib/click-help-colors
+from click             import command            # https://click.palletsprojects.com/en/7.x/#documentation
+from click_help_colors import HelpColorsCommand  # https://github.com/click-contrib/click-help-colors
 
-@click.command(
+@command(
     context_settings   = {'help_option_names': ['-h', '--help']},
-    cls                = click_help_colors.HelpColorsCommand,
+    cls                = HelpColorsCommand,
     help_headers_color = 'cyan',
     help_options_color = 'bright_white'
 )
