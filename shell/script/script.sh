9d5629e4ab2f07d203fb1277a2209178b7e6b585
Thorsten Kampe <thorsten@thorstenkampe.de>
2020-12-18 18:59:13 +0100

    + added option parsing (again) - function main for testing removed

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -6,12 +6,24 @@ shopt -os errexit errtrace nounset pipefail
 shopt -s dotglob failglob inherit_errexit
 export LANG=en_US.UTF-8  # neutral environment
 
-# MAIN CODE STARTS HERE #
-function main {
-    :
+PATH=/usr/bin:/bin:$PATH  # Cygwin: standard POSIX paths first
+
+# fix for `getopts x:` and `script -x -y` (getopts sees `-y` as argument for `-x`)
+function is_argument_option {
+    if [[ $OPTARG == -* ]]
+    then
+        echo "$0: option requires an argument -- $option" > /dev/stderr
+        exit 1
+    fi
 }
 
-if [[ ${BASH_SOURCE[0]} == "$0" ]]
-then
-    main "$@"
-fi
+# OPTIONS #
+while getopts '' option
+do
+    case $option in
+        ('?')
+            exit 1
+    esac
+done
+
+# MAIN CODE STARTS HERE #
