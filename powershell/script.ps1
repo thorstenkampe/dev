e347189f9e1c107ee4747e2f3737ca8526355035
Thorsten Kampe <thorsten@thorstenkampe.de>
2020-12-21 00:40:34 +0100

    logging implemented

--- powershell/script.ps1
+++ powershell/script.ps1
@@ -1,13 +1,28 @@
 # initialization #
-[CmdletBinding()]
+# enable debugging with `Set-PSDebug -Trace 1`
 Param(
-    [Parameter(Mandatory, ParameterSetName='Help')]
+    [Parameter(ParameterSetName='Help')]
     [Switch] $Help  # make help available without `Get-Help`
 )
 
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version latest
 [cultureinfo]::currentculture = [cultureinfo]::InvariantCulture  # "neutral" environment
+
+# logging #
+function log($Level, $Message) {
+    $color = @{
+        CRITICAL = 'Red'
+        ERROR    = 'DarkRed'
+        WARNING  = 'DarkYellow'
+        INFO     = 'DarkGreen'
+        DEBUG    = 'Gray'
+    }
+
+    Write-Color -Text $Level, ": $Message"  `
+                -Color $color[$Level], Gray `
+                -ShowTime
+}
 
 # options #
 if ($Help) {  # `-Help`
