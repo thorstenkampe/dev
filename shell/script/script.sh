2384acb6785fcec5e4a9513edb53202e7e28476b
Thorsten Kampe <thorsten@thorstenkampe.de>
2020-12-20 19:39:52 +0100

    parse all options to associative array

--- shell/script/script.sh
+++ shell/script/script.sh
@@ -8,22 +8,50 @@ export LANG=en_US.UTF-8  # neutral environment
 
 PATH=/usr/bin:/bin:$PATH  # Cygwin: standard POSIX paths first
 
-# fix for `getopts x:` and `script -x -y` (getopts sees `-y` as argument for `-x`)
-function is_argument_option {
-    if [[ $OPTARG == -* ]]
-    then
-        echo "$0: option requires an argument -- $option" > /dev/stderr
-        exit 1
-    fi
+# OPTIONS #
+_args=("$@")
+declare -A options
+function parse_options {
+    while getopts "$1" option "${_args[@]}"
+    do
+        case $option in
+            ('?')
+                exit 1
+                ;;
+
+            (*)
+                # fix for "getopts sees second option as argument for first option"
+                # (`getopts a:` and `script.sh -a -b`)
+                if [[ ${OPTARG-} == -* ]]
+                then
+                    echo "$0: option requires an argument -- $option" > /dev/stderr
+                    exit 1
+                fi
+
+                # shellcheck disable=SC2034
+                options[$option]=${OPTARG-}
+                ;;
+        esac
+    done
 }
 
-# OPTIONS #
-while getopts '' option
-do
-    case $option in
-        ('?')
-            exit 1
-    esac
-done
-
 # MAIN CODE STARTS HERE #
+# below is an example on how to use the parsed options
+parse_options a:bc
+
+# $ script.sh -a 1 -b
+# a: 1
+# b:
+# c: [not set]
+for option in a b c
+do
+    # shellcheck disable=SC2102
+    # `-v` for associative arrays in bash 4.3
+    if [[ -v options[$option] ]]
+    then
+        optarg=${options[$option]}
+    else
+        optarg='[not set]'
+    fi
+    echo "$option: $optarg"
+done
