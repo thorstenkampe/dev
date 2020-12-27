00d391d09d69f1cc96547d727b874a694f6ff53c
Thorsten Kampe <thorsten@thorstenkampe.de>
2020-12-28 00:56:01 +0100

    added logging

--- python/script/script.py
+++ python/script/script.py
@@ -1,4 +1,12 @@
-import click, click_help_colors
+import click, click_help_colors, colorlog
+
+# logging
+logger  = colorlog.getLogger()
+handler = colorlog.StreamHandler()
+
+fmt = '%(log_color)s%(levelname)s%(reset)s: %(message)s'
+handler.setFormatter(colorlog.ColoredFormatter(fmt=fmt, reset=False))
+logger.addHandler(handler)
 
 @click.command(
     context_settings   = {'help_option_names': ['-h', '--help']},
