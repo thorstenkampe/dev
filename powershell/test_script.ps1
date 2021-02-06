6ddd2653a3e5958880f7cb5d333468bbcc920483
Thorsten Kampe <thorsten@thorstenkampe.de>
2021-02-06 19:52:06 +0100

    Initial version

---
+++ powershell/test_script.ps1
@@ -0,0 +1,24 @@
+Describe 'Test script' {
+    It 'no option' {
+        $result   = ./script.ps1
+        $result   = $result.Split([Environment]::NewLine)[0]
+        $expected = 'script.ps1 [-Help] [<CommonParameters>]'
+
+        Assert-True -Actual $?
+        Assert-Equal -Actual $result -Expected $expected
+    }
+
+    It 'option help' {
+        $result   = ./script.ps1 -h
+        $result   = $result.Split([Environment]::NewLine)[0]
+        $expected = 'script.ps1 [-Help] [<CommonParameters>]'
+
+        Assert-True -Actual $?
+        Assert-Equal -Actual $result -Expected $expected
+    }
+
+    It 'unknown option' {
+        $exception = "A parameter cannot be found that matches parameter name 'x'."
+        {./script.ps1 -x}  | Should -Throw -ExpectedMessage $exception
+    }
+}
