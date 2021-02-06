e5db517feb24e6f668a877e737cc64ab04d51dd5
Thorsten Kampe <thorsten@thorstenkampe.de>
2021-02-06 20:02:36 +0100

    Initial version

---
+++ powershell/test/toolbox.Tests.ps1
@@ -0,0 +1,81 @@
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+Param()
+
+Import-Module -Name PesterMatchHashtable
+
+BeforeAll {
+    . $PSScriptRoot/../toolbox.ps1
+
+    function Get-TypeName($object) {
+        $object.GetType().Name
+    }
+}
+
+# dmap
+Describe 'dmap' {
+    BeforeEach {
+        . $PSScriptRoot/../test.ps1
+    }
+
+    It 'dmap' {
+        $result = @{a='Int32'; b='Int32'; c='Int32'; d='Int32'; e='Int32'; f='Int32'; g='Int32'; h='Int32'; '9'='String'}
+
+        dmap $hashtable -Keyfunc Get-TypeName
+        $hashtable | Should -MatchHashtable $result
+    }
+}
+
+# dupdate
+Describe 'dupdate' {
+    BeforeEach {
+        . $PSScriptRoot/../test.ps1
+    }
+
+    It 'dupdate' {
+        $result = @{a=1; b=2; c=3; d=4; e=5; f=6; g=7; h=8; i=9; '9'='j'}
+
+        dupdate $hashtable @{i=9; '9'='j'}
+        $hashtable | Should -MatchHashtable $result
+    }
+}
+
+# groupby
+Describe 'groupby' {
+    BeforeEach {
+        . $PSScriptRoot/../test.ps1
+    }
+
+    It 'array' {
+        $grouped = groupby $array -keyfunc Get-TypeName
+
+        $grouped['Int32']  | Should -Be 1, 2, 3, 4, 5, 6, 7, 8
+        $grouped['String'] | Should -Be 'i'
+    }
+
+    It 'hashtable' {
+        $grouped = groupby $ordered -keyfunc {param($x) Get-TypeName $x.Value}
+
+        $grouped['Int32'].Name   | Should -Be 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
+        $grouped['Int32'].Value  | Should -Be 1, 2, 3, 4, 5, 6, 7, 8
+        $grouped['String'].Name  | Should -Be 9
+        $grouped['String'].Value | Should -Be 'i'
+    }
+}
+
+# log
+Describe 'log' {
+    It 'info message' {
+        $verbosity = 'WARNING'
+        $result = log INFO 'test message'
+
+        Assert-Equal -Actual $result -Expected $null
+    }
+
+    It 'verbosity info message' {
+        $verbosity = 'INFO'
+        $result    = log INFO 'test message'
+        $expected  ='INFO: test message'
+
+        Assert-Equal -Actual $result -Expected $expected
+    }
+}
---
+++ powershell/toolbox.ps1
@@ -0,0 +1,72 @@
+﻿#  - ident #
+function ident($x) {
+    $x
+}
+
+#  - ConvertTo-Ordered #
+function ConvertTo-Ordered($Hash) {
+    # Shallow copy of the original (see comment for dmap)
+    $dict = [ordered]@{}
+    $keys = $Hash.Keys | Sort-Object
+
+    foreach ($key in $keys) {
+        $dict[$key] = $Hash[$key]
+    }
+
+    $dict
+}
+
+#  - dmap #
+function dmap($hash, $Keyfunc) {
+    # Modifies the original. Cloning a hash is shallow and not supported for ordered
+    # dictionaries. Copying with "(foreach key {clone[key] = orig[key]})" is also
+    # shallow
+    foreach ($key in @($hash.Keys)) {
+        $hash[$key] = & $Keyfunc $hash[$key]
+    }
+}
+
+#  - dupdate #
+function dupdate($hash1, $hash2) {
+    if ($hash2) {
+        foreach ($key in $hash2.Keys) {
+            $hash1.$key = $hash2.$key
+        }
+    }
+}
+
+#  - groupby #
+# https://www.powershellmagazine.com/2013/12/23/simplifying-data-manipulation-in-powershell-with-lambda-functions/
+function groupby($object, $keyfunc='ident') {
+    # * `groupby $array {param($x) $x.gettype().Name}`
+    # * `groupby $hashtable {param($x) $x.Value.GetType().Name}`
+    $object.GetEnumerator() | Group-Object -Property {& $keyfunc $PSItem} -AsHashTable
+}
+
+# - public_ip_address #
+function public_ip_address {
+    $public_url = 'http://v4.ipv6-test.com/api/myip.php'
+    (Invoke-WebRequest -Uri $public_url).Content
+}
+
+# - external_ip_address #
+function external_ip_address {
+    $interfaces = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration `
+                                  -Property DefaultIPGateway, IPAddress        `
+                                  -Filter 'IPEnabled = 1'
+
+    foreach ($interface in $interfaces) {
+        if ($interface.DefaultIPGateway) {
+            $interface.IPAddress[0]
+            break
+        }
+    }
+}
+
+function log($Level, $Message) {
+    $loglevel = @{CRITICAL = 50; ERROR = 40; WARNING = 30; INFO = 20; DEBUG = 10}
+
+    if ($loglevel[$Level] -ge $loglevel[$verbosity]) {
+        Write-Output -InputObject "${Level}: $Message"
+    }
+}
