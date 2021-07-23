89444fa5fcc18fd4a07d953307f93fa9ee024711
Thorsten Kampe <thorsten@thorstenkampe.de>
2021-07-23 19:19:35 +0200

    Change default log level to 'INFO'

--- python/script/script.py
+++ python/script/script.py
@@ -5,13 +5,14 @@ from rich import traceback
 
 traceback.install(width=80, extra_lines=1)
 
+# https://github.com/Delgan/loguru/issues/428
 def formatter(record):
     level_labels = {'SUCCESS': 'SUCC', 'WARNING': 'WARN', 'CRITICAL': 'CRIT'}
     level_name   = record['level'].name
     level        = level_labels.get(level_name, level_name)
     return f'<level>[{level} {{time:YYYY-MM-DD HH:mm:ss}}]</> {{message}}\n'
 
-def configure_logging(fmt=formatter, level='WARNING'):
+def configure_logging(fmt=formatter, level='INFO'):
     logger.configure(handlers=[dict(sink=sys.stderr, format=fmt, level=level)])
 
 configure_logging()
