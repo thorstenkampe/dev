d0d8132d37140ff87d3e9da4c1641838133b941b
Thorsten Kampe <thorsten@thorstenkampe.de>
2021-10-03 21:45:40 +0200

    refactor logging

--- python/script/script.py
+++ python/script/script.py
@@ -1,16 +1,16 @@
-import sys
+import os, sys
 import click, click_help_colors
 from loguru import logger
 from rich   import traceback
 
 traceback.install(width=80, extra_lines=1)
 
-logfmt = '<level>[{level} {time:YYYY-MM-DD HH:mm:ss}]</> {message}\n'
-
-def configure_logging(fmt=logfmt, level='INFO'):
-    logger.configure(handlers=[dict(sink=sys.stderr, format=fmt, level=level)])
-
-configure_logging()
+if os.isatty(2):
+    timestamp = ''
+else:
+    timestamp = ' {time:YYYY-MM-DD HH:mm:ss}'
+logfmt = f'<level>[{{level}}{timestamp}]</> {{message}}\n'
+logger.configure(handlers=[dict(sink=sys.stderr, format=logfmt, level='INFO')])
 
 @click.command(
     context_settings   = {'help_option_names': ['-h', '--help']},
