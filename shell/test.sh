15d9bf0f830ea64ba9751283ecd2450921757bbf
Thorsten Kampe <thorsten@thorstenkampe.de>
2021-10-08 20:26:19 +0200

    add integer variable

--- shell/test.sh
+++ shell/test.sh
@@ -1,10 +1,12 @@
 # shellcheck disable=SC2034
 
 string='The quick brown fox jumps over the lazy dog'
+declare -gi int=123
 
 # https://mywiki.wooledge.org/BashGuide/Arrays
-array=(1 2 3 4 5 6 7 8 9)  # show all: echo ${array[@]}
+# show all: `declare -p array`
+array=(1 2 3 4 5 6 7 8 9)
 
 declare -gA assoc
-# show all: declare -p assoc
+# show all: `declare -p assoc`
 assoc=([a]=1 [b]=2 [c]=3 [d]=4 [e]=5 [f]=6 [g]=7 [h]=8 [9]=9)
