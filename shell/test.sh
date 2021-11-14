1c92d3bd61529cace9c4519f02c23936c1b19d03
Thorsten Kampe <thorsten@thorstenkampe.de>
2021-11-14 14:06:40 +0100

    add empty element in arrays

--- shell/test.sh
+++ shell/test.sh
@@ -7,8 +7,8 @@ int=123
 
 # https://mywiki.wooledge.org/BashGuide/Arrays
 # show all: `declare -p array`
-array=(1 2 3 4 5 6 7 8 9)
+array=(1 2 3 4 5 6 7 8 '')
 
 declare -gA assoc
 # show all: `declare -p assoc`
-assoc=([a]=1 [b]=2 [c]=3 [d]=4 [e]=5 [f]=6 [g]=7 [h]=8 [9]=9)
+assoc=([a]=1 [b]=2 [c]=3 [d]=4 [e]=5 [f]=6 [g]=7 [h]=8 [9]='')
