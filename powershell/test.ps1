c6590493b48816254d680f152414a02698e63935
Thorsten Kampe <thorsten@thorstenkampe.de>
2021-11-27 14:42:48 +0100

    add syntax information for array and hashtable

--- powershell/test.ps1
+++ powershell/test.ps1
@@ -1,7 +1,10 @@
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_arrays
+# arrays can use commas (`1, ...`, `@(1, ...)`) and semicolons (`@(1; ...)`), semicolons
+# only within `@()`
 $array     = 1, 2, 3, 4, 5, 6, 7, 8, 'i'
 
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_hash_tables
+# hashtables can only use semicolons (`@{1; ...}`)
 # ordered hashtables can be accessed via key as well as numerical index
 $ordered   = [ordered]@{a=1; b=2; c=3; d=4; e=5; f=6; g=7; h=8; '9'='i'}
 $hashtable =          @{a=1; b=2; c=3; d=4; e=5; f=6; g=7; h=8; '9'='i'}
