3c043568b81442af33a3d65666ba7af72a394137
Thorsten Kampe <thorsten@thorstenkampe.de>
2021-12-24 23:46:33 +0100

    align test objects with the Bash shell ones

--- powershell/test/toolbox.Tests.ps1
+++ powershell/test/toolbox.Tests.ps1
@@ -21,14 +21,14 @@ Describe 'tb_map' {
     }
 
     It 'array' {
-        $result = 'Int32', 'Int32', 'Int32', 'Int32', 'Int32', 'Int32', 'Int32', 'Int32', 'String'
+        $result = 'Int32', 'Int32', 'Int32', 'Int32', 'Int32', 'Int32', 'String', 'String', 'String'
 
         tb_map -Keyfunc Get-TypeName $array
         ,$array | Should -MatchArrayOrdered $result
     }
 
     It 'hashtable' {
-        $result = @{a='Int32'; b='Int32'; c='Int32'; d='Int32'; e='Int32'; f='Int32'; g='Int32'; h='Int32'; '9'='String'}
+        $result = @{a='Int32'; b='Int32'; c='Int32'; d='Int32'; e='Int32'; f='Int32'; g='String'; 'h h'='String'; 9='String'}
 
         tb_map -Keyfunc Get-TypeName $hashtable
         $hashtable | Should -MatchHashtable $result
@@ -56,17 +56,17 @@ Describe 'tb_groupby' {
     It 'array' {
         $grouped = tb_groupby -keyfunc Get-TypeName $array
 
-        $grouped['Int32']  | Should -Be 1, 2, 3, 4, 5, 6, 7, 8
-        $grouped['String'] | Should -Be 'i'
+        $grouped['Int32']  | Should -Be 1, 2, 3, 4, 5, 6
+        $grouped['String'] | Should -Be '7', '8 8', ''
     }
 
     It 'hashtable' {
         $grouped = tb_groupby -keyfunc {param($x) Get-TypeName $x.Value} $ordered
 
-        $grouped['Int32'].Name   | Should -Be 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
-        $grouped['Int32'].Value  | Should -Be 1, 2, 3, 4, 5, 6, 7, 8
-        $grouped['String'].Name  | Should -Be 9
-        $grouped['String'].Value | Should -Be 'i'
+        $grouped['Int32'].Name   | Should -Be 'a', 'b', 'c', 'd', 'e', 'f'
+        $grouped['Int32'].Value  | Should -Be 1, 2, 3, 4, 5, 6
+        $grouped['String'].Name  | Should -Be 'g', 'h h', 9
+        $grouped['String'].Value | Should -Be '7', '8 8', ''
     }
 
     It 'case sensitive' {
