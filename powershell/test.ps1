552a6fe055b0a1c3d445ce8ae21417901039f344
Thorsten Kampe <thorsten@thorstenkampe.de>
2022-04-30 15:50:29 +0200

    remove whitespace

--- powershell/test.ps1
+++ powershell/test.ps1
@@ -8,7 +8,7 @@ $array     = 1, 2, 3, 4, 5, 6, '7', '8 8', ''
 # * hashtables can only use semicolons (`@{a=1; ...}`)
 # * .clone() a hash is shallow. Copying hashes with "(foreach key {clone[key] = orig[key]})"
 #   is also shallow
-$hashtable =          @{a=1; b=2; c=3; d=4; e=5; f=6; 'g'='7'; 'h h'='8 8'; 9=''}
+$hashtable = @{a=1; b=2; c=3; d=4; e=5; f=6; 'g'='7'; 'h h'='8 8'; 9=''}
 
 # * .clone() an ordered dictionary is not supported
 # * ordered dictionaries can be accessed via key as well as numerical index; key
