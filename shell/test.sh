3e91c94244b1f0eebf2cfecbae492748eea60317
Thorsten Kampe <thorsten@thorstenkampe.de>
2022-12-24 10:16:04 +0100

    add `test_` prefix

--- shell/test.sh
+++ shell/test.sh
@@ -8,7 +8,7 @@ declare -gi test_int=123
 # show array elements: `declare -p test_array`
 test_array=( [0]=1 [1]=2 [2]=3 [3]=4 [4]=5 [5]=6 [6]=7 [7]='8 8' [9]='' )  # this is a sparse array (no `${array[8]}`)
 
-# show array elements: `declare -p assoc`
+# show array elements: `declare -p test_assoc`
 declare -gA test_assoc=( [a]=1 [b]=2 [c]=3 [d]=4 [e]=5 [f]=6 [g]=7 [h h]='8 8' [9]='' )
 
 declare -gn test_nameref=test_assoc
